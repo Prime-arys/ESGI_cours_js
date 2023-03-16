19171c1773c47227271fcc5b0eba702cf1a97a12
Prime-arys <88397986+Prime-arys@users.noreply.github.com>
2023-03-16 09:10:32 +0100

    TP : fetch logic

--- tp/code.js
+++ tp/code.js
@@ -1,6 +1,27 @@
 const btn = document.getElementById('btn');
 const img1 = document.getElementById('img1');
 
+
+function fetch_neko(){
+    return fetch('https://nekos.best/api/v2/neko');
+}
+
+
 btn.addEventListener('click', ()=>{
     console.log('click');
+    const a = fetch_neko()
+        .then(response => response.json())
+        .then((json) => {
+            console.log(json.results[0].url)
+            img1.src = json.results[0].url;
+        })
+    
 })
+
+
+
+
+/*fetch('https://nekos.best/api/v2/neko')
+  .then(response => response.json())
+  .then(json => console.log(json.results[0].url))
+  */
