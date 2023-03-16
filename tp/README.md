c8a8548cc818a50f08adf31e5fe0c467dfad353a
Prime-arys <88397986+Prime-arys@users.noreply.github.com>
2023-03-16 09:43:08 +0100

    TP : + de détails

--- tp/README.md
+++ tp/README.md
@@ -1,2 +1,8 @@
 # TP
 
+#### API
+NekoBEST API: https://nekos.best/api/v2/neko
+<br>
+NekoBEST API Docs: https://nekos.best/api/v2/docs
+<br>
+_renvoie une image de "neko girls_
--- tp/code.js
+++ tp/code.js
@@ -1,22 +1,37 @@
 const btn = document.getElementById('btn');
 const img1 = document.getElementById('img1');
+const source = document.getElementById('source');
+const artist_name = document.getElementById('artist_name');
+const artist_url = document.getElementById('artist_url');
+
 
 
 function fetch_neko(){
-    return fetch('https://nekos.best/api/v2/neko');
+    return fetch('https://nekos.best/api/v2/neko'); // on appel l'api de neko.best
 }
 
 
+function neko(){
+    const neko_apl = fetch_neko()
+            .then(response => response.json()) //on recupère les données
+            .then((json) => {
+                console.log(json.results[0].artist_href)
+                img1.src = json.results[0].url; //on définit l'url de l'image retourné
+                source.textContent = json.results[0].artist_href;
+                artist_name.textContent = json.results[0].artist_name;
+                artist_url.textContent = json.results[0].source_url;
+            })
+}
+
+neko(); //première image au chargement de la page
+
+// lorsque que le bouton est cliqué
 btn.addEventListener('click', ()=>{
     console.log('click');
-    const a = fetch_neko()
-        .then(response => response.json())
-        .then((json) => {
-            console.log(json.results[0].url)
-            img1.src = json.results[0].url;
-        })
+    neko();
     
 })
+
 
 
 
